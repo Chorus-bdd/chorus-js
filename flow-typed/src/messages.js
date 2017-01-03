b7e9f3834ad8a66c35e8c053b1a6a62548ee5dd2
Benoît Grélard <benoit.grelard@gmail.com>
2017-01-03 14:31:42 +0000

    Make message object types strict

--- flow-typed/src/messages.js
+++ flow-typed/src/messages.js
@@ -1,42 +1,42 @@
 /* eslint-disable no-unused-vars */
 
-type ConnectMessage = {
+type ConnectMessage = {|
 	type: 'CONNECT',
 	chorusClientId: string,
 	description: string,
-}
+|}
 
-type PublishStepMessage = {
+type PublishStepMessage = {|
 	type: 'PUBLISH_STEP',
 	chorusClientId: string,
 	stepId: string,
 	pattern: string,
 	pendingMessage?: string,
 	technicalDescription?: string,
-}
+|}
 
-type StepsAlignedMessage = {
+type StepsAlignedMessage = {|
 	type: 'STEPS_ALIGNED',
 	chorusClientId: string,
-}
+|}
 
-type StepSucceededMessage = {
+type StepSucceededMessage = {|
 	type: 'STEP_SUCCEEDED',
 	chorusClientId: string,
 	stepId: string,
 	executionId: string,
 	result?: number | string | void,
 	contextVariables: Object,
-}
+|}
 
-type StepFailedMessage = {
+type StepFailedMessage = {|
 	type: 'STEP_FAILED',
 	chorusClientId: string,
 	stepId: string,
 	executionId: string,
 	description?: string,
 	errorText?: string,
-}
+|}
 
 type OutgoingMessage =
 	ConnectMessage |
@@ -45,7 +45,7 @@ type OutgoingMessage =
 	StepSucceededMessage |
 	StepFailedMessage;
 
-type ExecuteStepMessage = {
+type ExecuteStepMessage = {|
 	type: 'EXECUTE_STEP',
 	chorusClientId: string,
 	stepId: string,
@@ -54,4 +54,4 @@ type ExecuteStepMessage = {
 	arguments: Array<string>,
 	timeoutPeriodSeconds: number,
 	contextVariables: Object,
-}
+|}
