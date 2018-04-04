2de069ded6dbaf549d771d2e554d6aa3d0cd1929
Nick Ebbutt <nick@ebbutt.net>
2018-04-04 22:44:47 +0100

    Fix e2e tests Dockerize output

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -16,12 +16,19 @@ fi
 # Wait until the other services are reachable before running the chorus command
 DOCKERIZE="dockerize -wait tcp://chrome:5900 -wait tcp://hub:4444 -wait http://chorus-js-example-app:80 -timeout 90s"
 
+docker-compose exec chorus-interpreter ${DOCKERIZE}
+if [ $? -ne 0 ] ; then
+  echo "Failed Dockerize checks"
+  exit 1
+fi
+
+
 echo "Executing chorus tests..."
 for feature in ../features/*.feature
 do
 	echo "Executing feature: ${feature}..."
 	#-T detaches the terminal which has the effect of turning the console highlighting off
-	docker-compose exec -T chorus-interpreter ${DOCKERIZE} chorus -f /features/${feature} | tee ${feature}.actual.txt
+	docker-compose exec -T chorus-interpreter chorus -f /features/${feature} | tee ${feature}.actual.txt
 
 	echo "Diffing output..."
 	diff ${feature}.expected.txt ${feature}.actual.txt
