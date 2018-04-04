387f52cf8a3fe7127a06ca96dba751915d9b263e
Nick Ebbutt <nick@ebbutt.net>
2018-04-04 22:29:21 +0100

    Tidy up e2e.sh script, improve output on comparison fail.

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -15,30 +15,13 @@ fi
 
 # Wait until the other services are reachable before running the chorus command
 DOCKERIZE="dockerize -wait tcp://chrome:5900 -wait tcp://hub:4444 -wait http://chorus-js-example-app:80 -timeout 90s"
-#docker-compose exec -T chorus-interpreter ${DOCKERIZE} chorus -c -f /features
-
 
 echo "Executing chorus tests..."
 for feature in ../features/*.feature
 do
 	echo "Executing feature: ${feature}..."
+	#-T detaches the terminal which has the effect of turning the console highlighting off
 	docker-compose exec -T chorus-interpreter ${DOCKERIZE} chorus -f /features/${feature} | tee ${feature}.actual.txt
-
-	# We have to pick up the exit code using docker inspect
-    LAST_CONTAINER_ID=`docker ps -l -q`
-    EXIT_CODE=`docker inspect ${LAST_CONTAINER_ID} --format='{{.State.ExitCode}}'`
-
-    if [ "${EXIT_CODE}" -ne 0 ] ; then
-	  echo "Exit code from Chorus interpreter was $?"
-	  echo "Service logs:"
-	  docker-compose logs
-	  echo "Tests Failed"
-	  exit 1
-	fi
-
-#	LINE_COUNT=`wc -l ${feature}.expected.txt | awk '{ print $1 }'`
-
-#	tail -${LINE_COUNT}l ${feature}.actual.txt > ${feature}.actual.tail.txt
 
 	echo "Diffing output..."
 	diff ${feature}.expected.txt ${feature}.actual.txt
@@ -48,6 +31,13 @@ do
 		echo "Chorus interpreter output has not changed. ($feature)"
 	else
 	    echo "Actual Chorus interpreter output was different from expected! ($feature)"
+	    echo "Actual ------------------->"
+	    cat ${feature}.actual.txt
+		echo "---------------------------"
+		echo ""
+	    echo "Expected ------------------>"
+	    cat ${feature}.expected.txt
+	    echo "----------------------------"
 	    exit 1
     fi
 
