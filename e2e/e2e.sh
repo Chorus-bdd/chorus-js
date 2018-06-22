606083cff62c97bc39d5a189e235733abf5f0824
Nick Ebbutt <nick@ebbutt.net>
2018-06-22 15:26:02 +0100

    Move the feature files under ./e2e and tidy up the test output

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -6,28 +6,40 @@ cleanup() {
     docker-compose stop
 }
 
-echo "Starting Docker services..."
+echo "
+  Starting Docker services using docker-compose...
+  We are starting containers based on the following images:
+
+  1. chorusbdd/chorus-interpreter, to run the chorus interpreter
+  2. selenium/hub and selenium/node-chrome-debug, so that chorus can open a browser
+  3. node, to build and serve the back end for the app under test
+
+"
+
 docker-compose up -d
 if [ $? -ne 0 ] ; then
   echo "Failed to start Docker services"
   exit 1
 fi
 
-# Wait until the other services are reachable before running the chorus command
+echo "Started docker compose - now testing all the containers are reachable.."
+echo "This may take a little time..."
 DOCKERIZE="dockerize -wait tcp://chrome:5900 -wait tcp://hub:4444 -wait http://chorus-js-example-app:80 -timeout 90s"
-
 docker-compose exec chorus-interpreter ${DOCKERIZE}
 if [ $? -ne 0 ] ; then
-  echo "Failed Dockerize checks"
+  echo "Failed Dockerize checks, not all the containers were responsive - exiting"
   exit 1
 fi
-
+echo "Everything looks good..."
 
 echo "Executing chorus tests..."
-for feature in ../features/*.feature
+cd features
+for feature in *.feature
 do
 	echo "Executing feature: ${feature}..."
-	#-T detaches the terminal which has the effect of turning the console highlighting off
+
+	# exec -T detaches the terminal which has the effect of turning Chorus' console highlighting off
+	# Since we capturing and comparing the std output we need this param, or the console colour highlighting and animation will fail the tests
 	docker-compose exec -T chorus-interpreter chorus -f /features/${feature} | tee ${feature}.actual.txt
 
 	echo "Diffing output..."
@@ -35,7 +47,8 @@ do
 	DIFF_EXIT_CODE=$?
 
 	if [ ${DIFF_EXIT_CODE} -eq 0 ]; then
-		echo "Chorus interpreter output has not changed. ($feature)"
+		echo "PASSED - Chorus interpreter output has not changed. ($feature)"
+		echo ""
 	else
 	    echo "Actual Chorus interpreter output was different from expected! ($feature)"
 	    echo "Actual ------------------->"
